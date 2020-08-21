8c22100e70a2627806b400ebb9d82894d3cf2fa9
BiswajitThakur <biswajit733127@gmail.com>
2020-08-21 20:37:38 +0530

    commit 2

---
+++ stylish_text/js/style_text_2.js
@@ -0,0 +1,48 @@
+'use strict';
+try { !function(){
+window.document.getElementById('input1').oninput=()=>{
+document.getElementById('outp1').innerHTML=`${rplc(document.getElementById('input1').value,st1)}`;
+document.getElementById('outp2').innerHTML=`${rplc(document.getElementById('input1').value,st2)}`;
+document.getElementById('outp3').innerHTML=`${rplc(document.getElementById('input1').value,st3)}`;
+document.getElementById('outp4').innerHTML=`${rplc(document.getElementById('input1').value,st4)}`;
+document.getElementById('outp5').innerHTML=`${rplc(document.getElementById('input1').value,st5)}`;
+document.getElementById('outp6').innerHTML=`${rplc(document.getElementById('input1').value,st6)}`;
+document.getElementById('outp7').innerHTML=`${rplc(document.getElementById('input1').value,st7)}`;
+document.getElementById('outp8').innerHTML=`${rplc(document.getElementById('input1').value,st8)}`;
+document.getElementById('outp9').innerHTML=`${rplc(document.getElementById('input1').value,st9)}`;
+document.getElementById('outp10').innerHTML=`${rplc(document.getElementById('input1').value,st10)}`;
+document.getElementById('outp11').innerHTML=`${rplc(document.getElementById('input1').value,st11)}`;
+document.getElementById('outp12').innerHTML=`${rplc(document.getElementById('input1').value,st12)}`;
+document.getElementById('outp13').innerHTML=`${rplc(document.getElementById('input1').value,st13)}`;
+document.getElementById('outp14').innerHTML=`${rplc(document.getElementById('input1').value,st14)}`;
+document.getElementById('outp15').innerHTML=`${rplc(document.getElementById('input1').value,st15)}`;
+document.getElementById('outp16').innerHTML=`${rplc(document.getElementById('input1').value,st16)}`;
+document.getElementById('outp17').innerHTML=`${rplc(document.getElementById('input1').value,st17)}`;
+document.getElementById('outp18').innerHTML=`${rplc(document.getElementById('input1').value,st18)}`;
+document.getElementById('outp19').innerHTML=`${rplc(document.getElementById('input1').value,st19)}`;
+document.getElementById('outp20').innerHTML=`${rplc(document.getElementById('input1').value,st20)}`;
+document.getElementById('outp21').innerHTML=`${rplc(document.getElementById('input1').value,st21)}`;
+document.getElementById('outp22').innerHTML=`${
+(function(val){
+    let v="";
+    if (val.length>0){
+       for (let a=(val.length-1);a>=0;a--){
+          v+=val[a];
+       }
+     } else {};
+   return v;
+ }(rplc(document.getElementById('input1').value,st22)))
+	}`;
+document.getElementById('outp23').innerHTML=`${rplc(document.getElementById('input1').value,st23)}`;
+document.getElementById('outp24').innerHTML=`${rplc(document.getElementById('input1').value,st24)}`;
+document.getElementById('outp25').innerHTML=`${rplc(document.getElementById('input1').value,st25)}`;
+document.getElementById('outp26').innerHTML=`${rplc(document.getElementById('input1').value,st26)}`;
+document.getElementById('outp27').innerHTML=`${rplc(document.getElementById('input1').value,st27)}`;
+document.getElementById('outp28').innerHTML=`${rplc(document.getElementById('input1').value,st28)}`;
+document.getElementById('outp29').innerHTML=`${rplc(document.getElementById('input1').value,st29)}`;
+document.getElementById('outp30').innerHTML=`${rplc(document.getElementById('input1').value,st30)}`;
+document.getElementById('outp31').innerHTML=`${rplc(document.getElementById('input1').value,st31)}`;
+document.getElementById('outp32').innerHTML=`${rplc(document.getElementById('input1').value,st32)}`;
+document.getElementById('outp33').innerHTML=`${rplc(document.getElementById('input1').value,st33)}`;
+
+};}();} catch (error){};
